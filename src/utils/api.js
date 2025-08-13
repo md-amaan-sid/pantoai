ee1c0c9d826632f6e556c7ee148df4822e607884
md-amaan-sid <siddiquei.amaan@gmail.com>
2025-08-13 15:23:54 +0530

    update backend url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000", 
-  withCredentials: true,            
+  baseURL: "https://gitmanagement-backend.onrender.com",
+  withCredentials: true,
 });
 
 export default api;
